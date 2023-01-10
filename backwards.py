7b72ec54b76549a45a3a4a0164bd790b0037fbd8
jessicamarycooper <jessicarumbelow@gmail.com>
2023-01-10 17:23:53 +0000

    debugged wandb, refactoring

--- backwards.py
+++ backwards.py
@@ -2,10 +2,13 @@ import torch
 from matplotlib import pyplot as plt
 from IPython import display
 import numpy as np
+import pandas as pd
 import argparse
 import json
 import wandb
+import os
 from helpers import *
+os.environ["WANDB_SILENT"] = "true"
 
 
 # Here's the key function that optimises for a sequence of input embeddings, given a target_output string:
@@ -13,7 +16,7 @@ def optimise_input(model_name,
                    epochs=100, 
                    lr=0.1, 
                    rand_after=False,    # Do we re-initialise inputs tensor with random entries when an optimal input is found?
-                   w_freq=20,           # logging (write) frequency
+                   w_freq=10,           # logging (write) frequency
                    base_input=False,      # If False, start_inputs will be entirely random
                    batch_size=20, 
                    input_len=3, 
@@ -42,7 +45,8 @@ def optimise_input(model_name,
     # "return_tensors='pt'" ensures that we get a tensor in PyTorch format
 
     optimised_inputs = set()
-    optimised_inputs_table = wandb.Table(columns=['Optimised Input'] + [tokenizer.decode(t) for t in output_ix])
+    #table_columns =['Optimised Input'] + [tokenizer.decode(t) for t in output_ix]
+    optimised_inputs_table = pd.DataFrame()
 
     if output_len == None or output_len < output_ix.shape[0]:                    # This won't generally be the case, but if we don't specify output_len (i.e. it's == None), then...
         output_len = output_ix.shape[0]       # ...it will be set to the number of tokens in the encoding of the string 'target_output'
@@ -120,7 +124,21 @@ def optimise_input(model_name,
             # target_logits now of shape (batch_size, output_len)
             # output_len < output_ix.shape[0] was dealt with in line 133
             
-        token_dist = torch.stack([torch.stack([closest_tokens(e, word_embeddings, tokenizer)[2].squeeze(-1) for e in input[b]]) for b in range(batch_size)])
+        
+        token_dist, closest_ix = [],[]
+
+        for b in input:
+            tds, cixs = [], []
+            for be in b:
+                _, cix, td, _ = closest_tokens(be, word_embeddings, tokenizer)
+                tds.append(td)
+                cixs.append(cix)
+            token_dist.append(torch.stack(tds))
+            closest_ix.append(torch.stack(cixs))
+
+        token_dist, closest_ix = torch.stack(token_dist).squeeze(-1), torch.stack(closest_ix).squeeze(-1)
+
+
         # As far as I can tell, this creates a tensor of shape (batch_size, input_len, 1) which gives distance to nearest
         # legal token embedding for each input embedding in each batch
         mean_token_dist = token_dist.mean() * dist_reg
@@ -144,11 +162,6 @@ def optimise_input(model_name,
         batch_loss = loss.mean()
 
         total_loss = torch.stack([mean_token_dist, batch_loss, perp_loss]).mean()
-        wandb.log({'Total Loss':total_loss, 'Mean Token Distance': mean_token_dist, 'Mean Loss': batch_loss, 'Mean Perplexity Loss':perp_loss})
-
-        closest_ix = torch.stack([torch.stack([closest_tokens(e, word_embeddings, tokenizer)[1] for e in b]) for b in input]).squeeze(-1)
-        # This is similar to above, but building a tensor of indices of nearest embeddings, rather than distances.
-        # Iterates over batches, and for each batch iterates over embeddings, giving tensor of shape (batch_size, input_len).
 
         model_outs = model.generate(closest_ix, max_length = output_len+input_len)
         # The 'closest_ix' tensor is passed as the initial input sequence to the model, 
@@ -161,22 +174,22 @@ def optimise_input(model_name,
         # Each element of the tensor will be a sequence of tokens with a length of at most output_len+input_len.
         
         for b in range(batch_size):
-            output = tokenizer.decode(model_outs[b][input_len:])
-            if target_output in output and output not in optimised_inputs:
+            if target_output in tokenizer.decode(model_outs[b][input_len:]) and tokenizer.decode(model_outs[b][:input_len]) not in optimised_inputs:
                 done = tokenizer.decode(model_outs[b][:input_len])
                 optimised_inputs.add(done)
-                optimised_inputs_table.add_data(done, *loss[b].detach().cpu().numpy().tolist())
-                wandb.log({'Optimised Inputs': optimised_inputs_table})
+                optimised_inputs_table = optimised_inputs_table.append(pd.Series([done] + loss[b].detach().cpu().numpy().tolist()), ignore_index=True)
 
             if done is not None and rand_after:
                 input.data[b] = torch.rand_like(input[b])
                 # Random re-initialisation (if 'rand_after' set to True)
-
-  
+        
         if ((e+1) % w_freq == 0) or done and return_early:
         # Every w epochs we write to log, unless we have found an optimised input before that and 'return_early' == True. 
         # I'm still not entirely sure about the idea of 'return_early'.
-            print("Optimised Inputs:", optimised_inputs))
+
+            wandb.log({'Optimised Inputs': optimised_inputs_table, 'Total Loss':total_loss, 'Mean Token Distance': mean_token_dist, 'Mean Loss': batch_loss, 'Mean Perplexity Loss':perp_loss, 'Epoch':e, 'LR':optimiser.param_groups[0]['lr'], 'Num Inputs Found':len(optimised_inputs)})
+             
+            print("Optimised Inputs:", optimised_inputs)
             print('{}/{} Output Loss: {} Emb Dist Loss: {} Perp Loss: {} LR: {}'.format(e+1, epochs, batch_loss, mean_token_dist, perp_loss, optimiser.param_groups[0]['lr']))
             if verbose == 3:
                 print('Target Probs: {}\nTarget Logits: {}\nInput Dists: {}\nInput Perplexity: {}\n'.format(target_probs.detach().cpu().numpy(), target_logits.detach().cpu().numpy(), token_dist.detach().cpu().numpy(), perp.detach().reshape(-1).cpu().numpy()))
@@ -191,11 +204,6 @@ def optimise_input(model_name,
                         # these get joined with separator '' (SHOULDN'T THAT BE ' '?)  
                     print(b, repr(' Closest embeddings: {}'.format(tokenizer.decode(model_outs[b]), '\n')))
                         # WON'T THIS give string decodings of the embeddings, rather than the embeddings themselves?
-                else:
-                    print(repr(tokenizer.decode(model_outs[b])), end=' ')
-                    # The least verbose printed output. The 'end' parameter is used to specify the end-of-line string that is appended to the output. 
-                    # By default, this is a newline character, but in this case it has been set to a single space character, 
-                    # so the output will be separated by spaces rather than newlines.
 
             if done and return_early:
                 print('\nOptimised Input: "{}"'.format(done))
@@ -217,12 +225,13 @@ def optimise_input(model_name,
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--wandb_user', type=str, default='jessicamarycooper')
     parser.add_argument('--model_name', type=str, default='gpt2')
     parser.add_argument('--epochs', type=int, default=100)
     parser.add_argument('--lr', type=float, default=0.1)
-    parser.add_argument('--rand_after', type=bool, default=False)
-    parser.add_argument('--w_freq', type=int, default=20)
-    parser.add_argument('--base_input', type=bool, default=False)
+    parser.add_argument('--rand_after', action='store_true')
+    parser.add_argument('--w_freq', type=int, default=10)
+    parser.add_argument('--base_input', action='store_true')
     parser.add_argument('--batch_size', type=int, default=20)
     parser.add_argument('--input_len', type=int, default=3)
     parser.add_argument('--target_output', type=str, default='.')
@@ -231,12 +240,12 @@ if __name__ == '__main__':
     parser.add_argument('--perp_reg', type=int, default=0)
     parser.add_argument('--loss_type', type=str, default='log_prob_loss')
     parser.add_argument('--seed', type=int, default=0)
-    parser.add_argument('--return_early', type=bool, default=False)
+    parser.add_argument('--return_early', action='store_true')
     parser.add_argument('--verbose', type=int, default=1)
-    parser.add_argument('--lr_decay', type=bool, default=False)
+    parser.add_argument('--lr_decay', action='store_true')
     parser.add_argument('--noise_coeff', type=float, default=0.01)
     parser.add_argument('--note', type=str, default='')
-    parser.add_argument('--run_test_set', type=bool, default=True)
+    parser.add_argument('--run_test_set', action='store_true')
     args = parser.parse_args()
 
     test_set = {" 4 5 6":3," D E F":3," a lot of data":3," the water":5," is that the government will":7," Jupiter, Saturn, Uranus,":7," np":4," y_1)":10," , quod est,":11}
@@ -247,12 +256,12 @@ if __name__ == '__main__':
             args.target_output = to
             args.input_len = il
 
-            run = wandb.init(config=args, project='backwards', entity='jessicamarycooper', reinit=True)
+            run = wandb.init(config=args, project='backwards', entity=args.wandb_user, reinit=True)
             results = optimise_input(**vars(args))
             run.finish()
 
     else:
-        run = wandb.init(config=args, project='backwards', entity='jessicamarycooper', reinit=True)
+        run = wandb.init(config=args, project='backwards', entity=args.wandb_user, reinit=True)
         optimise_input(**vars(args))
         run.finish()
 
