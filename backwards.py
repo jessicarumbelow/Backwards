7531650a8ef908228e44bf4c757533edf7af4984
matthewrwatkins <secretsofcreation@gmail.com>
2023-01-18 13:03:32 +0000

    Tidied comments, etc.

--- backwards.py
+++ backwards.py
@@ -21,7 +21,7 @@ def optimise_input(model,
                    lr=0.1, 
                    rand_after=False,    # Do we re-initialise inputs tensor with random entries when an optimal input is found?
                    w_freq=10,           # logging (write) frequency
-                   base_input=False,      # If False, start_inputs will be entirely random
+                   base_input=False,      # If False, start_inputs will be entirely random, if true cluster centroids get used.
                    batch_size=20, 
                    input_len=3, 
                    target_output='.',    # Default target output is the "." token; this won't generally be used
@@ -31,16 +31,17 @@ def optimise_input(model,
                    loss_type='log_prob_loss', 
                    seed=0,
                    return_early=True,    # finishes if single optimised input is found
-                   verbose=1,            # Controls how much info gets logged.
+                   verbose=1,            # Controls how much info gets printed.
                    lr_decay=False,       # Use learning rate decay? If so, a scheduler gets invoked.
-                   run_random=0,
+                   run_random=0,         #
                    **kwargs): 
 
     torch.manual_seed(args.seed)
 
+    # Picks a single token at random from vocabulary for target_output
     if run_random > 0:
         random_ix = (torch.rand(1)*word_embeddings.shape[0]).int()
-        target_output = tokenizer.decode(random_ix)
+        target_output = tokenizer.decode(random_ix)  # Converts token index to string representation
         wandb.config.update({'target_output':target_output}, allow_val_change=True)
     
     print('Optimising input of length {} to maximise output logits for "{}"'.format(input_len, target_output))
@@ -55,30 +56,26 @@ def optimise_input(model,
     optimised_inputs = set()
     metrics_table = wandb.Table(columns=['Input', 'Output', 'Loss','Perplexity', 'Distance'])
 
-    if output_len == None or output_len < output_ix.shape[0]:                    # This won't generally be the case, but if we don't specify output_len (i.e. it's == None), then...
+    if output_len == None or output_len < output_ix.shape[0]:   # If we don't specify output_len (i.e. it's == None), then...
         output_len = output_ix.shape[0]       # ...it will be set to the number of tokens in the encoding of the string 'target_output'
     else:
         possible_target_positions = torch.stack([torch.arange(0, output_ix.shape[0]) + i for i in range(output_len - output_ix.shape[0] + 1)])
-
-    # Typically this would print something like 'Optimising input of length 6 to maximise output logits for "KILL ALL HUMANS!"'.
+        # generates list of legal token positions within output ("sequentiality enforcer")
 
     if base_input == False:
         start_input = torch.rand(batch_size, input_len, word_embeddings.shape[-1]).to(device)
         # If no base_input is provided, we construct start_input as a random tensor 
-        # of shape (batch_size, input_len, embedding_dim) (embedding_dim = 768 for this GPT-2 model).
+        # of shape (batch_size, input_len, embedding_dim)
         start_input = normalise(start_input,[word_embeddings.min(dim=0)[0], word_embeddings.max(dim=0)[0]])
-        # We normalise this random tensor so that its minimum and maximum values correspond to those in the entire word_embeddings tensor
-        # This dispenses with whole swathes of "input space" which contain no legal token embeddings 
-        # (we're limiting ourselves to a kind of "hull" defined by the 50527 vocab tokens in the embedding space), 
-        # which is a sensible place to look for optimised inputs.
+        # We normalise this random tensor so that its minimum and maximum values correspond columnwise to those in the entire word_embeddings tensor
+        # This keeps each dimension of the starting embeddings within the range of the legal token embeddings. 
     else:
-
-        _, centroids = kkmeans(word_embeddings, batch_size, seed=42)
+        # Otherwise we use k-means clustering to find centroids as our start_input embeddings
+        _, centroids = kkmeans(word_embeddings, batch_size, seed=seed)
         start_input = torch.cat([centroids.unsqueeze(1)]*input_len, dim=1)
 
     input = torch.nn.Parameter(start_input, requires_grad=True)
-    # input is not a tensor, it's a Parameter object that wraps a tensor and adds additional functionality. 
-    # 'input.data' is used below
+    # input is Parameter object that wraps a tensor and adds additional functionality. 
     
     optimiser = torch.optim.Adam([input], lr=lr)
     # standard optimiser; note that it generally operates on a list of tensors, so we're giving it a list of one tensor; standard learning rate
@@ -86,10 +83,9 @@ def optimise_input(model,
     # this is used when loss hasn't improved for 20 timesteps; this scheduler will reduce the lr by a 'factor' of 0.5 when the 
     # validation loss stops improving for 'patience' (here 20) epochs, and will wait 'cooldown' (here 20) epochs before resuming normal operation.
 
-    # now we loop across training epochs
     for e in range(epochs):
-        logits, emb, perp = model_emb(model, torch.clamp(input, word_embeddings.min(), word_embeddings.max()), word_embeddings, output_len)
-        # Does forward pass on a 'clamped' version of the 'input' tensor (done to contain it within the 'hull' of the vocabulary within 'input space').
+        logits, emb, perp = model_emb(model, torch.clamp(input, word_embeddings.min(dim = 0), word_embeddings.max(dim = 0)), word_embeddings, output_len)
+        # Does forward pass on a 'clamped' version of the 'input' tensor (which constrains it for each dimension to the range of all token embeddings)
         # Iterates to produce an output of output_len tokens, 
         # returns: 'logits' = tensor of logits for output, of shape (batch_size, output_len, vocab_size)
         # 'emb': tensor of embeddings for input+output of shape (batch_size, input_len + output_len, embedding_dim); 
@@ -98,14 +94,17 @@ def optimise_input(model,
         # For each batch, output, converts the sequence of logits (of length 'vocab_size') in the 'logits' tensor to probabilities, using softmax
 
         logits = (logits - logits.min(dim=-1)[0].unsqueeze(-1)) / (logits.max(dim=-1)[0].unsqueeze(-1) - logits.min(dim=-1)[0].unsqueeze(-1))
-        # This appears to be normalising the logits for each batch/output embedding so they're all between 0 and 1... 
+        # This normalises the logits for each batch/output embedding so they're all between 0 and 1. 
         # This is for ease of visualisation.
 
-        perp_loss = perp.mean() 
+        perp_loss = perp.mean()  # across all elements in the batch
 
         if output_len > output_ix.shape[0]:
             target_logits = logits[:,possible_target_positions,output_ix].max(dim=1)[0]
             target_probs = probs[:,possible_target_positions,output_ix].max(dim=1)[0]
+            # for 'contains in' scenario
+            # find the position (among all legal positions) for the target output that gives highest logits
+            # and analogously with probs
 
         else:
             target_logits = logits[:,torch.arange(output_len), output_ix]
@@ -113,11 +112,9 @@ def optimise_input(model,
 
             # This handles case where output_len == output_ix.shape[0]
             # target_logits now of shape (batch_size, output_len)
-            # output_len < output_ix.shape[0] was dealt with in line 133
-            
-        
-        token_dist, closest_ix = [],[]
 
+        # consolidation so that closest_tokens only needs to be used in a single loop
+        token_dist, closest_ix = [],[]
         for b in input:
             tds, cixs = [], []
             for be in b:
@@ -127,16 +124,17 @@ def optimise_input(model,
             token_dist.append(torch.stack(tds))
             closest_ix.append(torch.stack(cixs))
 
+        # convert lists into tensors of shape (batch_size, input_len)
         token_dist, closest_ix = torch.stack(token_dist).squeeze(-1), torch.stack(closest_ix).squeeze(-1)
 
-        # As far as I can tell, this creates a tensor of shape (batch_size, input_len, 1) which gives distance to nearest
-        # legal token embedding for each input embedding in each batch
+        # Creates a tensor of shape (batch_size, input_len, 1) which gives distance to nearest
+        # legal token embedding for each input embedding in the batch
         mean_token_dist = token_dist.mean() 
 
         # There are currently four loss types, many more could be introduced.
         # log_prob_loss is the current default.
         if loss_type == 'logit_loss':
-            loss = 1-target_logits
+            loss = 1 - target_logits
         elif loss_type == 'log_prob_loss':
             loss = -torch.log(target_probs)
         elif loss_type == 'prob_loss':
@@ -147,36 +145,40 @@ def optimise_input(model,
             print(loss_type + 'is not implemented.')
             return 
 
-        batch_loss = loss.mean()
+        # loss is tensor of shape (batch_size,)
+        batch_loss = loss.mean()  # mean across batch
 
         total_loss = torch.stack([mean_token_dist * dist_reg, batch_loss, perp_loss * perp_reg]).mean()
 
         model_outs = model.module.generate(closest_ix, max_length = output_len+input_len)
+        # "module" is used because 'model' has been wrapped in DataParallel
         # The 'closest_ix' tensor is passed as the initial input sequence to the model, 
         # and the max_length parameter specifies the maximum length of the total sequence to generate.
-        # The output sequence will be terminated either when the end-of-sequence token is generated 
-        # or when the maximum length is reached, whichever occurs first.
-        # 
+        # The output sequence will be terminated when the maximum length is reached
+         
         # The output of the model.generate method will be a tuple containing the generated sequences and the model's internal states. 
         # The generated sequences will be stored in a tensor of shape (batch_size, output_len+input_len). 
-        # Each element of the tensor will be a sequence of tokens with a length of at most output_len+input_len.
+        # Each row of the tensor will be a sequence of token indices with a length of output_len+input_len.
         
         for b in range(batch_size):
             if target_output in tokenizer.decode(model_outs[b][input_len:]) and tokenizer.decode(model_outs[b]) not in optimised_inputs:
-                
-                done = tokenizer.decode(model_outs[b])
+            # 'and' clause deals with optimised inputs which have already been found
+
+                done = tokenizer.decode(model_outs[b])  # an optimised input
                 optimised_inputs.add(done)
                 metrics_table.add_data(*[tokenizer.decode(model_outs[b][:input_len]), tokenizer.decode(model_outs[b][input_len:])] + torch.stack([loss.squeeze(-1)[b], perp[b], token_dist.mean(dim=1)[b]], dim=-1).tolist())
                 wandb.log({'Optimised Inputs': wandb.Html(''.join(['<p>{}.{}</p>'.format(i, repr(s)) for i, s in enumerate(optimised_inputs)]))})
+                # logging same info twice in different formats for wandb usage
 
                 if rand_after:
-                    input.data[b] = torch.rand_like(input[b])
-                    # Random re-initialisation (if 'rand_after' set to True)
-        
+                    input.data[b] = normalise(torch.rand_like(input[b]),[word_embeddings.min(dim=0)[0], word_embeddings.max(dim=0)[0]])
+                    # Random re-initialisation (if 'rand_after' set to True) and normalisation
+
+
         if ((e+1) % w_freq == 0) or done and return_early:
         # Every w epochs we write to log, unless we have found an optimised input before that and 'return_early' == True. 
-        # I'm still not entirely sure about the idea of 'return_early'.
-             
+        # return_early == True means we're only interested in first optimised input.
+
             print("Optimised Inputs:", optimised_inputs)
             print('{}/{} Output Loss: {} Emb Dist Loss: {} Perp Loss: {} LR: {}'.format(e+1, epochs, batch_loss, mean_token_dist, perp_loss, optimiser.param_groups[0]['lr']))
             if verbose == 3:
@@ -205,11 +207,11 @@ def optimise_input(model,
         optimiser.zero_grad()
         total_loss.backward()
         optimiser.step()
-        # I assume these three lines are standard NN optimisation stuff?
-
+        # standard NN optimisation commands
+        
         if lr_decay:
             scheduler.step(total_loss)
-         # scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimiser, 'min', patience=20, cooldown=20, factor=0.5) gets used if lr_decay == True
+
         done = None
 
     return {'Metrics':metrics_table}
